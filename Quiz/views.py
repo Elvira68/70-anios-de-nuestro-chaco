938e7bba7dc4d8f2a3a8d7e68c79e1a953c39969
Jorge-Mansilla <mansillajorge65@gmail.com>
2021-08-22 08:31:54 -0300

    Templates set-up

--- Quiz/views.py
+++ Quiz/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def inicio(request):
+    context = {
+        'bienvenidoValue': 'Bienvenido'
+    }
+    return render(request, 'inicio.html', context)
