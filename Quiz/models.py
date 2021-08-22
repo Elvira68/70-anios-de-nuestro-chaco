d87e0c475118cb3ff5a9798dcfdd7b2381ae0a0f
Jorge-Mansilla <mansillajorge65@gmail.com>
2021-08-22 06:01:33 -0300

    Creación de preguntas y respuestas

--- Quiz/models.py
+++ Quiz/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Pregunta(models.Model):
+    texto = models.TextField(verbose_name='Contenido de la pregunta')
+
+    def __str__(self):
+        return self.texto
+
+class Respuesta(models.Model):
+    pregunta = models.ForeignKey(Pregunta, related_name='preguntas', on_delete=models.CASCADE)
+    correcta = models.BooleanField(verbose_name='¿Es la respuesta correcta?', default=False, null=False)
+    texto = models.TextField(verbose_name='Contenido de la respuesta')
+
+    def __str__(self):
+        return self.texto
