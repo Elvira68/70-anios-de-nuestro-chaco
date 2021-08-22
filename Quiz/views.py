0be696b503ea5bb468e376deee70d36d29afa115
Jorge-Mansilla <mansillajorge65@gmail.com>
2021-08-22 18:04:12 -0300

    Formulario de registro

--- Quiz/views.py
+++ Quiz/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import RegistroFormulario
 
 def inicio(request):
     context = {
         'bienvenidoValue': 'Bienvenido'
     }
     return render(request, 'inicio.html', context)
+
+def registro(request):
+    titulo = 'Crear una cuenta'
+
+    if request.method == 'POST':
+        form = RegistroFormulario(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('')
+    else:
+        form = RegistroFormulario()
+    
+    context = {
+        'form': form,
+        'titulo': titulo
+    }
+
+    return render(request, 'usuario/registro.html', context)
