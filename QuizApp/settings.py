7d4f2d8ae0d038ae0c113ba8c2e4691fd3a0e058
Jorge-Mansilla <mansillajorge65@gmail.com>
2021-09-03 00:11:50 -0300

    Error Corregido en Configuración de Static

--- QuizApp/settings.py
+++ QuizApp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'informatorio-quiz-app.herokuapp.com']
 
 
 # Application definition
@@ -138,9 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(BASE_DIR), 'static'),
-    )
+# STATICFILES_DIRS = (
+#     os.path.join(os.path.dirname(BASE_DIR), 'static'),
+#     )
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 MEDIA_URL  = '/media/'
